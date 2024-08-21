e5b57d4716d5098b8a33511f3b066c09df9247d0
jichenghan <jichenghan@gmail.com>
2024-08-21 18:46:50 +0800

    阿凡达

--- src/feishu/message_card.py
+++ src/feishu/message_card.py
@@ -83,7 +83,7 @@ def handle_image_card(image_info, img_key_list, prompt):
                     "tag": "button",
                     "text": {
                         "tag": "plain_text",
-                        "content": "重新生成"
+                        "content": "AI润色提示词-重新生成"
                     },
                     "type": "primary",
                     "value": {
