e13b6626103068f4aac8d295a18589d0b85cea02
jicheng-han <jicheng.han@cotticoffee.com>
2024-10-31 14:08:48 +0800

    9

--- src/main.py
+++ src/main.py
@@ -15,19 +15,8 @@ import logging
 from aiohttp import web
 import asyncio
 from aiohttp import web
-import os
-import logging
-from flask import Flask, request, jsonify
-import lark_oapi as lark
-from lark_oapi.adapter.flask import *
-from lark_oapi.api.im.v1 import *
-import json
-import threading
-from lark_oapi.adapter.flask import *
  
-# 获取环境变量
-ENCRYPT_KEY = app_config.APP_ENCRYPT_KEY
-VERIFICATION_TOKEN = app_config.APP_VERIFICATION_TOKEN
+
 # 注册事件处理器
 MessageReceiveEventHandler.set_callback(feishu_conf, route_im_message)
 set_card_callback(feishu_conf, action_im_message)
@@ -57,35 +46,19 @@ async def handle_webhook_card(path, headers, data):
     except Exception:
         pass
 
-# async def webhook_event(request):
-#     # print('模    块: main.py - webhook_event: 直接输入')
-#     data = await request.read()
-#     oapi_request = OapiRequest(
-#         uri=request.path, body=data, header=OapiHeader(request.headers)
-#     )
-#     event_data = await request.json()
-#     # 打印接收到的事件数据
-#     # print("Received event data:", event_data)
+async def webhook_event(request):
+    # print('模    块: main.py - webhook_event: 直接输入')
+    data = await request.read()
+    oapi_request = OapiRequest(
+        uri=request.path, body=data, header=OapiHeader(request.headers)
+    )
+    event_data = await request.json()
+    # 打印接收到的事件数据
+    # print("Received event data:", event_data)
 
-#     oapi_resp = handle_event(feishu_conf, oapi_request)
-#     # print(f"handle_webhook_event_oapi_request.body: {oapi_resp}")
-#     return web.json_response({"message": "OK"})
-
-# 处理事件webhook
-async def webhook_event(post):
-    # Handle URL verification
-    def event():
- 
-        
-        # 处理URL验证请求
-        if request.json and "challenge" in request.json:
-            challenge = request.json.get("challenge")
-            return jsonify({"challenge": challenge})
-        
-        # 处理其他事件
- 
-        return '', 200
-
+    oapi_resp = handle_event(feishu_conf, oapi_request)
+    # print(f"handle_webhook_event_oapi_request.body: {oapi_resp}")
+    return web.json_response({"message": "OK"})
 
 def app_main():
     app = web.Application()
